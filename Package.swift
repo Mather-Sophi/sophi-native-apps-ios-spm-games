34397051654095dcd3ad64c9f9e87f6167037b61
Siddhant Misra <88447294+SMisra-TGAM@users.noreply.github.com>
2026-03-11 12:09:02 -0400

    Fix path

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Paywall",
-            url: "https://github.com/Mather-Sophi/Mather-Sophi/sophi-native-apps-ios-spm-games/releases/download/v0.0.1/Paywall.xcframework.zip",
+            url: "https://github.com/Mather-Sophi/sophi-native-apps-ios-spm-games/releases/download/v0.0.1/Paywall.xcframework.zip",
             checksum: "e1427c65ff6d1bdd0b2cf0e49b13ad283f8d5f86b950e63fdf22e3df39c0643a"
         )
     ]
