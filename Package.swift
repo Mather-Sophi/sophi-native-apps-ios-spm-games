b2e00c484463c3608e809732db4e03e453643136
Siddhant Misra <88447294+SMisra-TGAM@users.noreply.github.com>
2026-03-20 15:43:42 -0400

    Add Paywall Kit

--- Package.swift
+++ Package.swift
@@ -1,20 +1,32 @@
-// swift-tools-version:5.3
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Paywall",
-    platforms: [
-        .iOS(.v12),
-    ],
+    name: "PaywallKit",
     products: [
-        .library(name: "Paywall", targets: ["Paywall"])
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "PaywallKit",
+            targets: ["PaywallKit"]
+        ),
     ],
     targets: [
         .binaryTarget(
-            name: "Paywall",
+            name: "PaywallBinary",
             url: "https://github.com/Mather-Sophi/sophi-native-apps-ios-spm-games/releases/download/v0.0.1/Paywall.xcframework.zip",
             checksum: "e1427c65ff6d1bdd0b2cf0e49b13ad283f8d5f86b950e63fdf22e3df39c0643a"
-        )
+        ),
+        .target(
+            name: "PaywallKit",
+            dependencies: ["PaywallBinary"],
+            path: "PaywallKit/Sources/PaywallKit"
+        ),
+        .testTarget(
+            name: "PaywallKitTests",
+            dependencies: ["PaywallKit"],
+            path: "PaywallKit/Tests/PaywallKitTests"
+        ),
     ]
 )
